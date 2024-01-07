25f2a11a91e6e559a8fce317f2dc790c81934955
James <mirageapache@gmail.com>
2024-01-07 14:00:15 +0800

    建立Footer元件

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,26 @@
 import { useState } from 'react';
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import HomePage from 'pages/HomePage';
 import Header from 'components/Header';
+import Footer from 'components/Footer';
+import HomePage from 'pages/HomePage';
 
 function App() {
   const [darkMode, setDarkMode] = useState('');
 
   return (
     <div className={`App font-mono ${darkMode} `}>
-      <div className="bg-white text-gray-900 dark:bg-gray-950 dark:text-gray-100">
-        <BrowserRouter>
-          <Header darkMode={darkMode} setDarkMode={setDarkMode} />
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            {/* <Route path="/about" element={<About />} /> */}
-          </Routes>
-        </BrowserRouter>
+      <div className="min-h-screen flex flex-col bg-white text-gray-900 dark:bg-gray-950 dark:text-gray-100">
+        <Header darkMode={darkMode} setDarkMode={setDarkMode} />
+        <main className="mb-auto flex-grow">
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              {/* <Route path="/about" element={<About />} /> */}
+            </Routes>
+          </BrowserRouter>
+        </main>
+        <Footer />
       </div>
     </div>
   );
