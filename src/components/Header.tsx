0e29429c53ac7941a9b7bef35dae82d891f9958e
jameswang14 <james.wang@settour.com.tw>
2024-01-24 17:37:23 +0800

    修改Header深色模式按鈕點選效果

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -27,11 +27,21 @@ type HeaderProps = {
 };
 
 function Header({ darkMode, setDarkMode }: HeaderProps) {
-  const [toggleMenu, setToggleMenu] = useState('translate-x-full'); // Menu animation
+  const [toggleMenuAnimation, setToggleMenuAnimation] = useState('translate-x-full'); // Toggle Menu 動畫效果
+  const [darkModeAnimation, setDarkModeAnimation] = useState('translate-y-0 opacity-100'); // 深色模式按鈕動畫效果
+  const [lightModeAnimation, setLightModeAnimation] = useState('translate-y-3 opacity-0'); // 明亮模式按鈕動畫效果
+  const [searchInputAnimation, setSearchInputAnimation] = useState('w-0 bg-white'); // 搜尋輸入框動畫效果
 
-  // Dark Mode
+  // 控制深色模式
   function handleDarkMode() {
     setDarkMode(darkMode === 'dark' ? '' : 'dark');
+    setDarkModeAnimation(darkMode === 'dark' ? 'translate-y-0 opacity-100 delay-300 ease-in' : 'translate-y-3 opacity-0 ease-in');
+    setLightModeAnimation(darkMode === 'dark' ? 'translate-y-3 opacity-0 ease-in' : 'translate-y-0 opacity-100 delay-300 ease-in');
+  }
+
+  // 控制搜尋輸入框
+  function SearchInputFocus(){
+    setSearchInputAnimation('')
   }
 
   return (
@@ -44,30 +54,23 @@ function Header({ darkMode, setDarkMode }: HeaderProps) {
           </a>
         </div>
         <nav className="flex items-center text-lg">
-          {/* Search btn */}
-          <button aria-label="search" type="button" className="mx-2">
-            <SearchIcon className="h-5 w-5 m-1.5 stroke-0 fill-gray-900 dark:fill-gray-100" />
-          </button>
-          {darkMode !== 'dark' ? (
-            <>
-              {/* DarkMode btn */}
-              <button aria-label="darkMode" type="button" className="mx-2" onClick={handleDarkMode}>
-                <DarkModeIcon className="h-9 w-9 rounded-full p-2 m-1.5 fill-gray-900 dark:fill-gray-100 hover:bg-gray-300" />
-              </button>
-            </>
-          ) : (
-            <>
-              {/* LightMode btn */}
-              <button
-                aria-label="lightMode"
-                type="button"
-                className="mx-2"
-                onClick={handleDarkMode}
-              >
-                <LightModeIcon className="h-9 w-9 rounded-full p-2 m-1.5 fill-gray-900 dark:fill-gray-100 hover:bg-gray-600" />
-              </button>
-            </>
-          )}
+          {/* 搜尋 */}
+          <div className='flex items-center relative border border-red-500'>
+            <input type="text" placeholder="搜尋..." className={`p-4 h-6 text-lg rounded-full ${searchInputAnimation}`} />
+            <button aria-label="search" type="button" className="absolute right-0">
+              <SearchIcon className="h-5 w-5 m-1.5 border stroke-0 fill-gray-900 dark:fill-gray-100" />
+            </button>
+          </div>
+          {/* 深色模式切換 */}
+          <div className="flex justify-center items-center relative">
+            <button aria-label="darkMode" type="button" className="mx-2" onClick={handleDarkMode}>
+              <DarkModeIcon className={`h-9 w-9 rounded-full p-2 m-1.5 fill-gray-900 dark:fill-gray-100 hover:bg-gray-300 transform duration-300 ${darkModeAnimation}`} />
+            </button>
+            <button aria-label="lightMode" type="button" className="mx-2 absolute" onClick={handleDarkMode}>
+              <LightModeIcon className={`h-9 w-9 rounded-full p-2 m-1.5 fill-gray-900 dark:fill-gray-100 hover:bg-gray-600 transform duration-300 ${lightModeAnimation}`} />
+            </button>
+          </div>
+          {/* 註冊/登入 */}
           <button
             type="button"
             className="hidden sm:inline-block rounded-full text-white bg-sky-500 hover:bg-sky-700 px-4 py-1 dark:bg-sky-800"
@@ -79,14 +82,14 @@ function Header({ darkMode, setDarkMode }: HeaderProps) {
             aria-label="toggleMenu"
             type="button"
             className="mx-2 sm:hidden"
-            onClick={() => setToggleMenu('translate-x-0')}
+            onClick={() => setToggleMenuAnimation('translate-x-0')}
           >
             <MenuIcon className="h-7 w-7 m-1 fill-gray-900 dark:fill-gray-100" />
           </button>
         </nav>
 
         <div
-          className={`fixed w-full h-full flex flex-col top-0 left-0 transform duration-300 ease-in-out ${toggleMenu} bg-white opacity-95 dark:bg-gray-950 dark:opacity-[0.98]`}
+          className={`fixed w-full h-full flex flex-col top-0 left-0 transform duration-300 ease-in-out ${toggleMenuAnimation} bg-white opacity-95 dark:bg-gray-950 dark:opacity-[0.98]`}
         >
           <div className="z-10 w-full flex justify-end p-4">
             {/* Close Menu btn */}
@@ -94,7 +97,7 @@ function Header({ darkMode, setDarkMode }: HeaderProps) {
               aria-label="close"
               type="button"
               className="pr-3"
-              onClick={() => setToggleMenu('translate-x-full')}
+              onClick={() => setToggleMenuAnimation('translate-x-full')}
             >
               <CloseIcon className="h-7 w-7 fill-gray-900 dark:fill-gray-100" />
             </button>
