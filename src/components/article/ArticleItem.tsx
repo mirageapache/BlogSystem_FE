3b88d75029614427027c3f2b601c56bb84cbad72
James <mirageapache@gmail.com>
2024-02-01 22:49:09 +0800

    package.json 調整

--- src/components/article/ArticleItem.tsx
+++ src/components/article/ArticleItem.tsx
@@ -32,7 +32,7 @@ function ArticleItem({ id, title, body, tags }: ArticleType) {
         <div className="mr-3">
           {isEmpty(true) ? (
             <span className="w-11 h-11 rounded-full flex justify-center items-center bg-sky-600 font-semibold">
-              <p className="text-xl text-center text-white">A</p>
+              <p className="text-xl text-center text-white">J</p>
             </span>
           ) : (
             <img className="w-11 h-11 rounded-full" src="" alt="author avatar" />
@@ -48,7 +48,9 @@ function ArticleItem({ id, title, body, tags }: ArticleType) {
           </div>
           <div>
             <h2 className="font-semibold text-2xl xl:text-3xl">
-              <Link to={`/article/${id}`}>{title}</Link>
+              <Link to={`/article/${id}`} className="hover:underline">
+                {title}
+              </Link>
             </h2>
             <div className="text-orange-500">{tagsList}</div>
             <p className="text-gray-600 dark:text-gray-300 line-clamp-3">{body}</p>
