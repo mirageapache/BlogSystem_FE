abd6c7dc9642c04e20aa2350a4e48eeca306c75d
James <mirageapache@gmail.com>
2024-05-22 22:05:44 +0800

    [optimize] 優化SideBar及搜尋頁

--- src/components/layout/SideBar.tsx
+++ src/components/layout/SideBar.tsx
@@ -1,4 +1,4 @@
-import { ReactNode } from 'react';
+import { ReactNode, useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { icon } from '@fortawesome/fontawesome-svg-core/import.macro';
 import { isEmpty } from 'lodash';
@@ -11,14 +11,22 @@ type ItemProps = {
   text: string;
   count: number;
   children: ReactNode;
+  activeItem: boolean;
+  changeItem: () => void;
 };
 
+const activeStyle = 'text-orange-500 hover:text-orange-500 hover:fill-orange-500';
+const normalStyle = 'text-gray-700 dark:text-gray-300 hover:text-orange-500';
+
 /** SideBar Item 元件 */
-function SideBarItem({ href, text, count, children }: ItemProps) {
+function SideBarItem({ href, text, count, children, activeItem, changeItem }: ItemProps) {
   return (
     <Link
       to={href}
-      className="flex my-1.5 text-xl text-gray-700 dark:text-gray-300 cursor-pointer hover:text-orange-500 hover:fill-orange-500 py-4"
+      className={`flex my-1.5 text-xl cursor-pointer py-4 ${
+        activeItem ? activeStyle : normalStyle
+      }`}
+      onClick={changeItem}
     >
       <span className="flex items-center">{children}</span>
       <span className="ml-3 font-bold hidden lg:block">
@@ -36,28 +44,65 @@ function SideBarItem({ href, text, count, children }: ItemProps) {
 /** SideBar 元件 */
 function SideBar() {
   const userId = getCookies('uid');
+  const [activeItem, setActiveItem] = useState('home'); // 顯示作用中的 Item
 
   return (
     <div className="text-left h-fit sm:px-1 px-5">
       <div className="ml-2.5">
-        <SideBarItem href="/" text="首頁" count={0}>
+        <SideBarItem
+          href="/"
+          text="首頁"
+          count={0}
+          activeItem={activeItem === 'home'}
+          changeItem={() => setActiveItem('home')}
+        >
           <FontAwesomeIcon icon={icon({ name: 'home' })} />
         </SideBarItem>
-        <SideBarItem href="/explore" text="探索" count={0}>
+        <SideBarItem
+          href="/explore"
+          text="探索"
+          count={0}
+          activeItem={activeItem === 'explore'}
+          changeItem={() => setActiveItem('explore')}
+        >
           <FontAwesomeIcon icon={icon({ name: 'compass', style: 'regular' })} />
         </SideBarItem>
-        <SideBarItem href="/search" text="搜尋" count={0}>
+        <SideBarItem
+          href="/search"
+          text="搜尋"
+          count={0}
+          activeItem={activeItem === 'search'}
+          changeItem={() => setActiveItem('search')}
+        >
           <FontAwesomeIcon icon={icon({ name: 'search', style: 'solid' })} />
         </SideBarItem>
         {checkLogin() && (
           <>
-            <SideBarItem href={`/profile/${userId}`} text="個人資料" count={0}>
+            <SideBarItem
+              href={`/profile/${userId}`}
+              text="個人資料"
+              count={0}
+              activeItem={activeItem === 'profile'}
+              changeItem={() => setActiveItem('profile')}
+            >
               <FontAwesomeIcon icon={icon({ name: 'user', style: 'regular' })} />
             </SideBarItem>
-            <SideBarItem href="/inbox" text="訊息匣" count={0}>
+            <SideBarItem
+              href="/inbox"
+              text="訊息匣"
+              count={0}
+              activeItem={activeItem === 'inbox'}
+              changeItem={() => setActiveItem('inbox')}
+            >
               <FontAwesomeIcon icon={icon({ name: 'inbox' })} />
             </SideBarItem>
-            <SideBarItem href="/activity" text="動態" count={0}>
+            <SideBarItem
+              href="/activity"
+              text="動態"
+              count={0}
+              activeItem={activeItem === 'activity'}
+              changeItem={() => setActiveItem('activity')}
+            >
               <FontAwesomeIcon icon={icon({ name: 'bell', style: 'regular' })} />
             </SideBarItem>
           </>
--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -10,7 +10,7 @@ import PageBanner from 'components/layout/PageBanner';
 // --- functions / types ---
 import { SearchStateType, setSearchText } from '../redux/searchSlice';
 // --- api / type ---
-import { ArticleResultType, getSearchArticle } from '../api/article';
+import { getPartialArticles, ArticleResultType, getSearchArticle } from '../api/article';
 
 /** stateType (SearchPage) */
 interface stateType {
@@ -21,13 +21,17 @@ function SearchPage() {
   const dispatch = useDispatch();
   const searchState = useSelector((state: stateType) => state.search);
   const { searchText } = searchState;
+  let articleQueryData: ArticleResultType;
 
-  // Article 文章資料
-  const articleQueryData = useQuery('aritcleList', () => getSearchArticle(searchText), {
-    enabled: false, // 禁用初始自動查詢
-  }) as ArticleResultType;
-
-  console.log(articleQueryData);
+  /** 預設取得文章 */
+  if (isEmpty(searchText)) {
+    articleQueryData = useQuery('articles', () => getPartialArticles(5)) as ArticleResultType;
+  } else {
+    // 搜尋 Article 文章資料
+    articleQueryData = useQuery('aritcleList', () => getSearchArticle(searchText), {
+      enabled: false, // 禁用初始自動查詢
+    }) as ArticleResultType;
+  }
 
   const { refetch } = articleQueryData;
   // handle search text change
@@ -38,34 +42,32 @@ function SearchPage() {
 
   return (
     <div className="max-w-[600px]">
-      <PageBanner title="搜尋" classname="flex justify-center">
-        <div className="relative flex items-center w-4/5 md:max-w-[480px] my-2">
-          <input
-            type="text"
-            name="search"
-            placeholder="搜尋..."
-            value={searchText}
-            onChange={(e) => {
-              dispatch(setSearchText(e.target.value));
-              handleSearchTextChange(e);
-            }}
-            className="p-4 pl-10 w-full h-9 text-lg rounded-full bg-gray-200 dark:bg-gray-700 outline-none"
-          />
-          <FontAwesomeIcon
-            icon={icon({ name: 'search', style: 'solid' })}
-            className="absolute h-5 w-5 m-1.5 ml-3 stroke-0 text-gray-500 dark:text-gray-100"
-          />
-          {/* 清除搜尋字串 */}
-          <FontAwesomeIcon
-            icon={icon({ name: 'xmark', style: 'solid' })}
-            onClick={() => {
-              dispatch(setSearchText(''));
-              refetch(); // 清除搜尋字串後重新發起查詢
-            }}
-            className="absolute right-0 h-5 w-5 m-1.5 mr-3 stroke-0 text-gray-500 dark:text-gray-100 cursor-pointer"
-          />
-        </div>
-      </PageBanner>
+      <div className="relative flex items-center w-full my-2">
+        <input
+          type="text"
+          name="search"
+          placeholder="搜尋..."
+          value={searchText}
+          onChange={(e) => {
+            dispatch(setSearchText(e.target.value));
+            handleSearchTextChange(e);
+          }}
+          className="p-4 pl-10 w-full h-9 text-lg rounded-full bg-gray-200 dark:bg-gray-700 outline-none"
+        />
+        <FontAwesomeIcon
+          icon={icon({ name: 'search', style: 'solid' })}
+          className="absolute h-5 w-5 m-1.5 ml-3 stroke-0 text-gray-500 dark:text-gray-100"
+        />
+        {/* 清除搜尋字串 */}
+        <FontAwesomeIcon
+          icon={icon({ name: 'xmark', style: 'solid' })}
+          onClick={() => {
+            dispatch(setSearchText(''));
+            refetch(); // 清除搜尋字串後重新發起查詢
+          }}
+          className="absolute right-0 h-5 w-5 m-1.5 mr-3 stroke-0 text-gray-500 dark:text-gray-100 cursor-pointer"
+        />
+      </div>
       <ArticleList articleQueryData={articleQueryData} />
     </div>
   );
