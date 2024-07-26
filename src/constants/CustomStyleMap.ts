5985577a84d7c0c62b152ab275adea7ad22a2075
jameswang14 <james.wang@settour.com.tw>
2024-07-26 17:38:33 +0800

    [feat] build customStyleMap bg-color

--- src/constants/CustomStyleMap.ts
+++ src/constants/CustomStyleMap.ts
@@ -1,15 +1,31 @@
 /** 自定義樣式 */
 export const customStyleMap = {
-  'BG-YELLOW': {
-    backgroundColor: 'yellow',
+  // 標示顏色
+  'BG-DEFAULT': {
+    backgroundColor: 'inherit',
+  },
+  'BG-GRAY': {
+    backgroundColor: 'gray',
   },
   'BG-RED': {
     backgroundColor: 'red',
+  },
+  'BG-ORANGE': {
+    backgroundColor: 'orange',
+  },
+  'BG-YELLOW': {
+    backgroundColor: 'yellow',
   },
   'BG-GREEN': {
     backgroundColor: 'green',
   },
   'BG-BLUE': {
     backgroundColor: 'blue',
+  },
+  'BG-INDIGO': {
+    backgroundColor: 'indigo',
+  },
+  'BG-PURPLE': {
+    backgroundColor: 'purple',
   },
 };
