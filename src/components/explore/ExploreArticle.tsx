06f8a5218f881daff3a928210814f753b0355f6a
James <mirageapache@gmail.com>
2024-09-04 21:48:34 +0800

    [feat] ExplorePage modify hashTag function

--- src/components/explore/ExploreArticle.tsx
+++ src/components/explore/ExploreArticle.tsx
@@ -15,7 +15,7 @@ function ExploreArticle() {
   const searchString = searchParams.get('search') || ''; // 取得搜尋字串
   let nextPage = -1; // 下一頁指標，如果為「-1」表示最後一頁了
 
-  // 使用 useInfiniteQuery 取得貼文
+  // 使用 useInfiniteQuery 取得文章
   const { data, fetchNextPage, isLoading } = useInfiniteQuery(
     ['exploreArticle', searchString],
     ({ pageParam = 1 }) =>
@@ -57,7 +57,7 @@ function ExploreArticle() {
   }, [nextPage]);
 
   if (get(data, 'pages[0].code', undefined) === 'NO_FOUND')
-    return <NoSearchResult msgOne="搜尋不到相關貼文" msgTwo="" type="article" />;
+    return <NoSearchResult msgOne="搜尋不到相關文章" msgTwo="" type="article" />;
 
   if (!isEmpty(data) && get(data, 'code', undefined) === 'ERR_NETWORK')
     return <BasicErrorPanel errorMsg="與伺服器連線異常，請稍候再試！" />;
--- src/components/explore/ExploreHashTag.tsx
+++ src/components/explore/ExploreHashTag.tsx
@@ -53,10 +53,13 @@ function ExploreHashTag() {
     return () => window.removeEventListener('scroll', handleScroll);
   }, [nextPage]);
 
-  if (get(data, 'pages[0].code', undefined) === 'NO_FOUND')
+  if (get(data, 'pages[0].code', undefined) === 'NO_SEARCH_STRING')
     return (
       <NoSearchResult msgOne="輸入貼文的HashTag" msgTwo="即可搜尋你想找的主題貼文" type="post" />
     );
+
+  if (get(data, 'pages[0].code', undefined) === 'NO_FOUND')
+    return <NoSearchResult msgOne="搜尋不到相關HashTag貼文" msgTwo="" type="post" />;
 
   if (!isEmpty(data) && get(data, 'code', undefined) === 'ERR_NETWORK')
     return <BasicErrorPanel errorMsg="與伺服器連線異常，請稍候再試！" />;
