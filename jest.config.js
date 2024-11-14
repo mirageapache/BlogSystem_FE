114381c1c03394194a3a30abb8814633b7d04632
jameswang14 <james.wang@settour.com.tw>
2024-11-14 15:38:43 +0800

    完成Post List 測試

--- jest.config.js
+++ jest.config.js
@@ -6,6 +6,8 @@ module.exports = {
   moduleNameMapper: {
     '\\.(gif|ttf|eot|svg|png)$': '<rootDir>/test/__mocks__/fileMock.js',
     '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
+    '^constants/(.*)$': '<rootDir>/src/constants/$1',
   },
   setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
+  moduleDirectories: ['node_modules', 'src'],
 };
---
+++ src/test/PostList.test.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import PostListDynamic from '../components/post/PostListDynamic';
+import PostItem from '../components/post/PostItem';
+import { mockPostData } from './mockData';
+
+jest.mock('../components/post/PostItem', () => jest.fn(() => <div>Post Item</div>));
+
+describe('測試PostList元件', () => {
+  test('測試貼文 loading 狀態', () => {
+    render(<PostListDynamic postListData={[]} isLoading atBottom={false} />);
+    const loadingElements = screen.getAllByLabelText('loading animation');
+    expect(loadingElements.length).toBeGreaterThan(0); // 確認至少有一個<PostLoading>
+  });
+
+  test('測試貼文清單顯示', () => {
+    render(<PostListDynamic postListData={mockPostData} isLoading={false} atBottom={false} />);
+    // 檢查渲染的 PostItem 元素數量是否與 mockPostData 長度一致
+    expect(screen.getAllByText('Post Item').length).toBe(mockPostData.length);
+
+    mockPostData.forEach((post) => {
+      // 驗證每個 PostItem 是否接收到正確的 props
+      expect(PostItem).toHaveBeenCalledWith({ postData: post }, {});
+    });
+  });
+
+  test('測試沒有更多貼文顯示', () => {
+    render(<PostListDynamic postListData={mockPostData} isLoading={false} atBottom />);
+    expect(screen.getByText('- 已經沒有更多貼文了 -')).toBeInTheDocument();
+  });
+});
